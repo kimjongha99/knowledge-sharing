85cc8dc4af883c4164fa0befb137c7dd6ca3f995
kimjongha99 <jjhsun99@gmail.com>
2024-01-25 16:31:57 +0900

    Setup, Feat: 메일인증 jwt환경설정 및 아이디 중복 검사기능

---
+++ knowledge-sharing/src/main/java/com/hanghae/knowledgesharing/controller/AuthController.java
@@ -0,0 +1,43 @@
+package com.hanghae.knowledgesharing.controller;
+
+
+import com.hanghae.knowledgesharing.dto.request.auth.EmailCertificationRequestDto;
+import com.hanghae.knowledgesharing.dto.request.auth.IdCheckRequestDto;
+import com.hanghae.knowledgesharing.dto.response.auth.EmailCertificationResponseDto;
+import com.hanghae.knowledgesharing.dto.response.auth.IdCheckResponseDto;
+import com.hanghae.knowledgesharing.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+
+    @PostMapping("/id-check")
+    public ResponseEntity<? super IdCheckResponseDto> idCheck(
+            @RequestBody @Valid IdCheckRequestDto requestBody
+    ) {
+        ResponseEntity<? super IdCheckResponseDto> response = authService.idCheck(requestBody);
+        return response;
+    }
+    @PostMapping("/email-certification")
+    public ResponseEntity<? super EmailCertificationResponseDto> emailCertification(
+            @RequestBody @Valid EmailCertificationRequestDto requestBody
+    ) {
+        ResponseEntity<? super EmailCertificationResponseDto> response = authService.emailCertification(requestBody);
+        return response;
+    }
+
+
+
+
+}
---
+++ knowledge-sharing/src/main/java/com/hanghae/knowledgesharing/entity/User.java
@@ -0,0 +1,31 @@
+package com.hanghae.knowledgesharing.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name="user")
+@Table(name="user")
+public class User {
+    @Id
+    @Column(name = "user_id", columnDefinition = "VARCHAR(30)")
+    private  String userId;
+    @Column(name = "password", columnDefinition = "VARCHAR(30)")
+    private  String password;
+    @Column(name = "email", columnDefinition = "VARCHAR(255)")
+    private String email;
+    @Column(name = "type", columnDefinition = "VARCHAR(10)")
+    private String type;
+    @Column(name = "role", columnDefinition = "VARCHAR(10)")
+    private String role;
+
+
+
+}
