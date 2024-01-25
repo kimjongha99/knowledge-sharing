0bb44721d0692c59d92a31cf5ee99948c136b38a
kimjongha99 <jjhsun99@gmail.com>
2024-01-25 16:18:38 +0900

    Setting: 초기세팅
    메일인증 , oauth 에대한 의존성 추가

---
+++ knowledge-sharing/src/test/java/com/hanghae/knowledgesharing/KnowledgeSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghae.knowledgesharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeSharingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
