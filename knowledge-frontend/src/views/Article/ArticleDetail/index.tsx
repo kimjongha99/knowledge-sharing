f5a95ddfab9cbc83b570092858fd031f36dfc889
kimjongha99 <jjhsun99@gmail.com>
2024-02-06 19:25:34 +0900

    Feat: [프론트] 조회수 api 통합

--- knowledge-frontend/src/views/Article/ArticleDetail/index.tsx
+++ knowledge-frontend/src/views/Article/ArticleDetail/index.tsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Comments from "./Comment";
+import {useCookies} from "react-cookie";
 
 // Define an interface to represent the response data structure
 interface ArticleResponse {
@@ -18,6 +19,9 @@ interface ArticleResponse {
 function ArticleDetail() {
     const { articleId } = useParams();
     const [articleDetail, setArticleDetail] = useState<ArticleResponse | null>(null); // Annotate with the interface
+    const [cookies] = useCookies();
+
+    const accessToken = cookies.accessToken; // Retrieving the access token from cookies
 
     useEffect(() => {
         const fetchArticleDetail = async () => {
@@ -32,9 +36,33 @@ function ArticleDetail() {
         fetchArticleDetail();
     }, [articleId]);
 
+
+
+    // New useEffect for incrementing view count
+    useEffect(() => {
+        const incrementViewCount = async () => {
+            try {
+                // Replace with the actual token if needed
+                const config = {
+                    headers: {
+                        Authorization: `Bearer ${accessToken}` // Including the token in the Bearer Authorization header
+                    }                };
+                await axios.get(`http://localhost:4040/api/v1/articles/${articleId}/increase-view`, config);
+            } catch (error) {
+                console.error('Error incrementing view count:', error);
+            }
+        };
+
+        if (articleId) {
+            incrementViewCount();
+        }
+    }, [articleId]);
+
     if (!articleDetail) {
         return <div>Loading...</div>;
     }
+
+
 
     // Render the article details using the articleDetail object
     return (
