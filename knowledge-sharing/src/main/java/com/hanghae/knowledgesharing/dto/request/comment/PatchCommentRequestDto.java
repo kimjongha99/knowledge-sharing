842e4842961736514dd477eec8082987183f1b19
kimjongha99 <jjhsun99@gmail.com>
2024-02-07 10:39:16 +0900

    patchDto생성

---
+++ knowledge-sharing/src/main/java/com/hanghae/knowledgesharing/dto/request/comment/PatchCommentRequestDto.java
@@ -0,0 +1,11 @@
+package com.hanghae.knowledgesharing.dto.request.comment;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PatchCommentRequestDto {
+}
