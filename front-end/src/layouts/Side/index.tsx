15aa20265c4adfce1a878878f5ed15b6adae5da5
kimjongha99 <jjhsun99@gmail.com>
2024-02-18 00:27:54 +0900

    feat:레아이웃 잡아놓

--- front-end/src/layouts/Side/index.tsx
+++ front-end/src/layouts/Side/index.tsx
@@ -1,16 +1,28 @@
 import './style.css';
 import {SetStateAction, useState} from "react";
 import {useCookies} from "react-cookie";
+import {useNavigate} from "react-router-dom";
+import {useUserStore} from "../../stores/userStore";
 
 export default function Side() {
     // 사이드바의 너비는250 높이는 헤더와 푸터 사이이다.
 // 활성 항목을 추적하는 상태
     const [activeIndex, setActiveIndex] = useState(0);
     const [cookies] = useCookies(['accessToken', 'refreshToken']); // Include refreshToken if needed
+    const navigate = useNavigate(); // Use the useNavigate hook
+    const { user, setUser } = useUserStore();
 
     // activeIndex 상태를 클릭한 항목의 인덱스로 업데이트합니다.
     const handleItemClick = (index: SetStateAction<number>) => {
         setActiveIndex(index);
+        if (index === 0) {
+            navigate(`/`);
+        }
+
+        if (index === 1 && isLoggedIn) {
+            navigate(`/user/${user?.userId}`);
+        }
+
     };
 
     const isLoggedIn = cookies.accessToken && cookies.refreshToken;
@@ -30,14 +42,14 @@ export default function Side() {
             <nav className="main-menu">
                 <ul>
                     <li className={`nav-item ${activeIndex === 0 ? 'active' : ''}`} onClick={() => handleItemClick(0)}>
-                        <a href="#">
+                        <a >
                             <i className="fa fa-house nav-icon"></i>
                             <span className="nav-text">Home</span>
                         </a>
                     </li>
                     {isLoggedIn &&(
                             <li className={`nav-item ${activeIndex === 1 ? 'active' : ''}`} onClick={() => handleItemClick(1)}>
-                                <a href="#">
+                                <a>
                                     <i className="fa fa-user nav-icon"></i>
                                     <span className="nav-text">Profile</span>
                                 </a>
@@ -45,19 +57,19 @@ export default function Side() {
                         )}
                         
                     <li className={`nav-item ${activeIndex === 2 ? 'active' : ''}`} onClick={() => handleItemClick(2)}>
-                        <a href="#">
+                        <a>
                             <i className="fa fa-calendar-check nav-icon"></i>
                             <span className="nav-text">Schedule</span>
                         </a>
                     </li>
                     <li className={`nav-item ${activeIndex === 3 ? 'active' : ''}`} onClick={() => handleItemClick(3)}>
-                        <a href="#">
+                        <a>
                             <i className="fa fa-person-running nav-icon"></i>
                             <span className="nav-text">Activities</span>
                         </a>
                     </li>
                     <li className={`nav-item ${activeIndex === 4 ? 'active' : ''}`} onClick={() => handleItemClick(4)}>
-                        <a href="#">
+                        <a >
                             <i className="fa fa-sliders nav-icon"></i>
                             <span className="nav-text">Settings</span>
                         </a>
--- front-end/src/views/MyPage/index.tsx
+++ front-end/src/views/MyPage/index.tsx
@@ -6,8 +6,19 @@ export default function MyPage(){
 
     return(
         <div>
-           TEST 보드를 만들것입니다.
-
+            <main id="Main">
+                <div id="main-left">
+                    <section id="main-left-one"></section>
+                    <section id="main-left-two"></section>
+                    <section id="main-left-three"></section>
+                </div>
+                <div id="main-right">
+                    <section id="main-right-one"></section>
+                    <section id="main-right-two"></section>
+                    <section id="main-right-three"></section>
+                    <section id="main-right-four"></section>
+                </div>
+            </main>
         </div>
     )
 }
