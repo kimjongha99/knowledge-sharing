53bfbfef813d5d9a9c020f94852aa215f0d2f7a4
kimjongha99 <jjhsun99@gmail.com>
2024-03-02 05:53:38 +0900

    hotfix: 생성자 실수

--- knowledge-sharing/src/main/java/com/hanghae/knowledgesharing/auth/dto/request/auth/IdCheckRequestDto.java
+++ knowledge-sharing/src/main/java/com/hanghae/knowledgesharing/auth/dto/request/auth/IdCheckRequestDto.java
@@ -3,10 +3,12 @@ package com.hanghae.knowledgesharing.auth.dto.request.auth;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
-public class IdCheckRequestDto {
+@NoArgsConstructor // 기본 생성자 추가
+ public class IdCheckRequestDto {
 
 
     @NotBlank
