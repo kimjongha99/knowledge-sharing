9c3a4f612b61b307ec9390eb888643f27b27bdb2
kimjongha99 <jjhsun99@gmail.com>
2024-03-02 06:01:16 +0900

    Update IdCheckRequestDto.java

--- knowledge-sharing/src/main/java/com/hanghae/knowledgesharing/auth/dto/request/auth/IdCheckRequestDto.java
+++ knowledge-sharing/src/main/java/com/hanghae/knowledgesharing/auth/dto/request/auth/IdCheckRequestDto.java
@@ -3,9 +3,11 @@ package com.hanghae.knowledgesharing.auth.dto.request.auth;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class IdCheckRequestDto {
 
 
