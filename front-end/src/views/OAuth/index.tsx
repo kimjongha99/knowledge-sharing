8a1d695005f53cde6271a96a0e939c85c2d6a84f
kimjongha99 <jjhsun99@gmail.com>
2024-03-06 12:04:47 +0900

    FIX: oauth refresh토큰까지

--- front-end/src/views/OAuth/index.tsx
+++ front-end/src/views/OAuth/index.tsx
@@ -3,14 +3,14 @@ import { useCookies } from 'react-cookie';
 import { useNavigate, useParams } from 'react-router-dom';
 
 export default function OAuth() {
-  const { accessToken, expirationTime } = useParams();
+  const { accessToken, refreshToken } = useParams();
 
-  const [cookie, setCookies] = useCookies(['accessToken']);
+  const [cookie, setCookies] = useCookies(['accessToken','refreshToken']);
   const navigate = useNavigate();
 
   useEffect(() => {
     // oauth 토큰 전달해주기 //
-    if (!accessToken || !expirationTime) return; // 유효하지 않은 토큰 정보는 처리하지 않습니다.
+    if (!accessToken || !refreshToken) return; // 유효하지 않은 토큰 정보는 처리하지 않습니다.
 
 
     // 액세스 토큰 쿠키 설정
@@ -21,19 +21,19 @@ export default function OAuth() {
       sameSite: 'none' // 쿠키가 모든 컨텍스트에서 전송되어야 함을 나타냅니다. 크로스 사이트 요청에 사용될 수 있습니다.
     });
 
-    //
-    // // 리프레시 토큰 쿠키 설정
-    // // 리프레시 토큰의 만료 시간은 일반적으로 서버 측에서 결정됩니다.
-    // // 여기서는 예시로 7일 후로 설정합니다. 실제 애플리케이션에서는 서버에서 받은 만료 시간을 사용하세요.
-    // setCookies('refreshToken', refreshToken, {
-    //   path: '/',
-    //   maxAge: 60 * 60 * 24 * 7, // 7일을 초 단위로 설정합니다.
-    //   secure: true,
-    //   sameSite: 'none'
-    // });
+
+    // 리프레시 토큰 쿠키 설정
+    // 리프레시 토큰의 만료 시간은 일반적으로 서버 측에서 결정됩니다.
+    // 여기서는 예시로 7일 후로 설정합니다. 실제 애플리케이션에서는 서버에서 받은 만료 시간을 사용하세요.
+    setCookies('refreshToken', refreshToken, {
+      path: '/',
+      maxAge: 60 * 60 * 24 * 7, // 7일을 초 단위로 설정합니다.
+      secure: true,
+      sameSite: 'none'
+    });
 
     navigate('/'); // 설정이 끝나면 홈페이지로 리다이렉트합니다.
-  }, [accessToken, expirationTime, navigate, setCookies]); // 의존성 배열에 포함된 변수들이 변경될 때마다 이펙트 실행
+  }, [accessToken, navigate, setCookies]); // 의존성 배열에 포함된 변수들이 변경될 때마다 이펙트 실행
 
   return (
     <>
--- knowledge-sharing/src/main/java/com/hanghae/knowledgesharing/auth/handler/OAuth2SuccessHandler.java
+++ knowledge-sharing/src/main/java/com/hanghae/knowledgesharing/auth/handler/OAuth2SuccessHandler.java
@@ -38,18 +38,15 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         user.setRefreshToken(refreshToken);
         userRepository.save(user);
 
-//        long refreshTokenExpiryDuration = 7 * 24 * 60 * 60;
-//
-        response.sendRedirect("https://knowledge-sharing-two.vercel.app/ "+ accessToken + "/3600");
-//        // 7일의 시간을 초단위로 환산
-//        long refreshExpirySeconds = 7 * 24 * 60 * 60; // 7 days in seconds
-//
-//        String redirectURL = String.format("https://knowledge-sharing-two.vercel.app/oauth-response/%s/3600/%s/%d",
-//                URLEncoder.encode(accessToken, StandardCharsets.UTF_8),
-//                URLEncoder.encode(refreshToken, StandardCharsets.UTF_8),
-//                refreshExpirySeconds);
-//
-//        response.sendRedirect(redirectURL);
+
+        String redirectUrl = String.format(
+                "https://knowledge-sharing-two.vercel.app/oauth-response/%s/%s",
+                accessToken,
+                refreshToken
+        );
+
+        response.sendRedirect(redirectUrl);
+
     }
 
 }
